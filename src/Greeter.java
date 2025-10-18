9599d06242a8cd4ce33741bea11f485d39880648
Igor Tacu <igortacu@users.noreply.github.com>
2025-10-18 22:48:04 +0300

    work in progress before rebase

--- src/Greeter.java
+++ src/Greeter.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Greeter {
+    private final String greeting; // helo
+    public Greeter( String greeting){
+        if(greeting == null || greeting.isEmpty()){
+            throw new IllegalArgumentException("Greeting is null or empty or times is less than 0");
+        }
+        this.greeting = greeting;
+    }
+    public String greet(String name) {
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("Name");
+        return greeting + ", " + name + "!";
+    }
+
+    public String greet(String name, int times) {
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("Name");
+        if (times <= 0) throw new IllegalArgumentException("times > 0");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < times; i++) {
+            if (i > 0) sb.append(System.lineSeparator());
+            sb.append(greet(name));
+        }
+        return sb.toString();
+    }
 }
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Greeter g = new Greeter("Nigga");
+        System.out.println(g.greet("Nigga", 1000));
+    }
+
+}
+
